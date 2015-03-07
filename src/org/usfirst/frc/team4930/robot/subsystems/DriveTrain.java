a0690185b3fd9ff5c80a4ae008110b340944afbd
JonKiddy <jon.kiddy@gmail.com>
2015-03-07 11:37:21 -0500

    Some drivetrain updates
    Student comments, etc.

--- src/org/usfirst/frc/team4930/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team4930/robot/subsystems/DriveTrain.java
@@ -1,39 +1,41 @@
 package org.usfirst.frc.team4930.robot.subsystems;
 
 import org.usfirst.frc.team4930.robot.RobotMap;
-import org.usfirst.frc.team4930.robot.commands.*;
-import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.command.*;
+import org.usfirst.frc.team4930.robot.commands.TeleOpTankDrive;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  * DriveTrain Subsystem
  * 
- * @author Thomas Kubiniec
- * speedController declarations
- * allocates DriveTrain Motors
- * PMW ports 0 and 9.
+ * @author Thomas Kubiniec : These are the declarations of each speedController,
+ *         telling them to allocate to each of the DriveTrain Motors in PMW
+ *         ports 0 and 9.
  *
  */
 public class DriveTrain extends Subsystem {
-	
-    SpeedController pWM0 = RobotMap.driveTrainPWM0;
-    SpeedController pWM9 = RobotMap.driveTrainPWM9;
-    RobotDrive robotDrive = RobotMap.driveTrainRobotDrive;
 
-    public void initDefaultCommand() {
-        setDefaultCommand(new TeleOpTankDrive());
-    }
-    
-    public void takeJoystickInputs(Joystick left, Joystick right){
-    	robotDrive.tankDrive(-left.getY(), -right.getY());
-    }
-    
-    public void runMotors(double left, double right){
-    	robotDrive.tankDrive(-left, -right);
-    }
-    
-    public void stop() {
-    	robotDrive.tankDrive(0, 0);
-    }
-    
+	SpeedController pWM0 = RobotMap.driveTrainPWM0;
+	SpeedController pWM9 = RobotMap.driveTrainPWM9;
+	RobotDrive robotDrive = RobotMap.driveTrainRobotDrive;
+
+	public void initDefaultCommand() {
+		setDefaultCommand(new TeleOpTankDrive());
+	}
+
+	public void takeJoystickInputs(Joystick left, Joystick right) {
+		robotDrive.tankDrive(-left.getY(), -right.getY());
+	}
+
+	public void runMotors(double left, double right) {
+		robotDrive.tankDrive(-left, -right);
+	}
+
+	public void stop() {
+		robotDrive.tankDrive(0, 0);
+	}
+
 }
